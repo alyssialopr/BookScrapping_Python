469cca25e31c0638349d84489b4fcf5278394eb1
alyssialopr <alyssia.pradon@gmail.com>
2024-10-25 20:18:13 +0200

    :zap: Phase 4 !! :zap:

--- category.py
+++ category.py
@@ -1,124 +1,99 @@
-from bs4 import BeautifulSoup
-import requests
-import csv
 import os
+import csv
+import requests
+from bs4 import BeautifulSoup
 
-if not os.path.exists('book_img'):
-    os.makedirs('book_img')
-
-# Phase 1 :  Récuperer les données d'un livre
 if not os.path.exists('csv'):
     os.makedirs('csv')
 
+url_home = 'http://books.toscrape.com/'
+url_catalogue = 'http://books.toscrape.com/catalogue/'
 
-url = 'http://books.toscrape.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-base_url = 'http://books.toscrape.com/' 
-
-url_home = 'http://books.toscrape.com/catalogue/'
-
-def get_book(soup, url):
-    product_page_url = url
-    response = requests.get(product_page_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+def get_book(book_url):
+    response = requests.get(book_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     book_data = soup.select('td')
 
-    upc = book_data[0].text
-    title = soup.h1.text
-    price_including_tax = soup.find_all('td')[3].text.replace('Â', '')
-    price_excluding_tax = soup.find_all('td')[2].text.replace('Â', '')
-    number_available = soup.find_all('td')[5].text
-    product_description = soup.find_all('p')[3].text
-    category = soup.find_all('a')[3].text
-    review_rating = soup.find_all("p", class_="star-rating")[0]['class'][1]
-    image_url = base_url + soup.select("img")[0]["src"].replace('../../', '')
-    # print(image_url)
-
-    return {
-        'product_page_url': product_page_url, 
-        'upc': upc, 
-        'title': title, 
-        'price_including_tax': price_including_tax, 
-        'price_excluding_tax': price_excluding_tax, 
-        'number_available': number_available, 
-        'product_description': product_description, 
-        'category': category,  
-        'review_rating': review_rating,
-        'image_url': image_url
-    }
-
-# get_book(soup, url)
-
-# Phase 2 : Récuperer les données de tout les livres d'une catégorie
-
-def download_images(X, filename):
-    # books_img_urls = [url + img["src"].replace('../../', '') for img in soup.select("img")]
-    # print(books_img_urls)
-    # for img_url in books_img_urls:
-    #     response = requests.get(img_url)
-        # filename = 'book_img/' + img_url.split('/')[-1]
-
+    # Vérification des éléments avant l'extraction
     try:
-        response = requests.get(str(X))
-        soup_image = BeautifulSoup(response.content, 'html.parser')
-        # print(soup_image)
-        print(response)
-        print('book_img/' + filename)
-        print(X)
-        with open('book_img/' + filename + '.jpg', 'wb') as file:
-            file.write(response.content)
-        print('Image downloaded')
-    except:
-        print('Error')
+        upc = book_data[0].text if len(book_data) > 0 else "N/A"
+        title = soup.h1.text if soup.h1 else "N/A"
+        price_including_tax = book_data[3].text.replace('Â', '') if len(book_data) > 3 else "N/A"
+        price_excluding_tax = book_data[2].text.replace('Â', '') if len(book_data) > 2 else "N/A"
+        number_available = book_data[5].text if len(book_data) > 5 else "N/A"
+        product_description = soup.select_one('#product_description ~ p').text if soup.select_one('#product_description ~ p') else "N/A"
+        category = soup.select('ul.breadcrumb li a')[-1].text.strip() if soup.select('ul.breadcrumb li a') else "N/A"
+        review_rating = soup.find('p', class_='star-rating')['class'][1] if soup.find('p', class_='star-rating') else "No rating"
+        image_url = url_home + soup.select_one('.item.active img')['src'].replace('../', '') if soup.select_one('.item.active img') else "N/A"
 
-def get_Books_from_category(soup, url, category_number):
-    url_category = url + soup.select('ul > li > a')[category_number]['href']
-    response = requests.get(url_category)
-    soup = BeautifulSoup(response.content, 'html.parser')
+        return {
+            'product_page_url': book_url, 
+            'upc': upc, 
+            'title': title, 
+            'price_including_tax': price_including_tax, 
+            'price_excluding_tax': price_excluding_tax, 
+            'number_available': number_available, 
+            'product_description': product_description, 
+            'category': category,  
+            'review_rating': review_rating,
+            'image_url': image_url
+        }
+    except Exception as e:
+        print(f"Erreur lors de l'extraction des données pour le livre {book_url}: {e}")
+        return None
+
+def get_Books_from_category(category_url):
+    response = requests.get(category_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     all_books = []
 
     while True:
-            
         books = soup.select('h3 > a')
         for book in books:
-            book_url = url_home + book['href'].replace('../../../', '')
-            book_data = get_book(soup, book_url)
-            all_books.append(book_data)
+            book_url = url_catalogue + book['href'].replace('../../../', '')
+            book_data = get_book(book_url)
+            if book_data:  # Ajouter le livre seulement s'il a des données
+                all_books.append(book_data)
 
-        next = soup.find('li', class_='next')
-        if next :
-            next_page_url = next.find('a')['href']
-            response = requests.get(url_category.replace('index.html', next_page_url))
-            soup = BeautifulSoup(response.content, 'html.parser')
-
-        else :
+        # Suivre le lien "Next" pour la page suivante
+        next_button = soup.find('li', class_='next')
+        if next_button:
+            next_page_url = url_home + next_button.find('a')['href']
+            response = requests.get(next_page_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+        else:
             break
 
-    with open('csv/category.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    return all_books
 
-        writer.writeheader()
-        for book in all_books:
-            writer.writerow(book)
-            download_images(book['image_url'], book['title'])
+def get_all_books_from_all_category():
+    response = requests.get(url_home)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    categories = soup.select('ul > li > ul > li > a')
 
+    for category in categories:
+        category_name = category.text.strip()
+        category_url = url_home + category['href']
 
-url = 'http://books.toscrape.com/'
+        # Assurer la création du dossier pour chaque catégorie
+        category_dir = 'csv/category'
+        if not os.path.exists(category_dir):
+            os.makedirs(category_dir)
+        category_filename = f'{category_dir}/{category_name}.csv'
 
-get_Books_from_category(soup, url, 4)
+        # Récupérer et enregistrer les données des livres dans la catégorie
+        print(f"Téléchargement de la catégorie : {category_name}")
+        all_books = get_Books_from_category(category_url)
 
+        with open(category_filename, 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(all_books)
 
-# Phase 3 : Récupérer les images de chaque livres de la même catégorie et les télécharger dans un dossier
+        print(f"{category_name} téléchargé avec succès")
 
-# books_img_urls = [url + img["src"].replace('../../', '') for img in soup.select("img")]
-# print(books_img_urls)
-
-# for book_img_url in books_img_urls:
-#     response = requests.get(book_img_url)
-#     # filename = 'book_img/' + book_img_url.split('/')[-1]
-#     # filename = 'book_img/' + book_title 
-#     # download_images(book_img_url, filename)
+# Lancer le processus
+get_all_books_from_all_category()
+print("Tous les livres ont été téléchargés avec succès !")
